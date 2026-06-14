a3a54707d59d3e88500048a82046e3f483b1c17c
Yami An <yamiannephilim@gmail.com>
2026-06-14 09:43:39 +0700

    260614

--- Ibaraki Pca Weight/Control/FrmUpdate.vb
+++ Ibaraki Pca Weight/Control/FrmUpdate.vb
@@ -1,16 +1,22 @@
-﻿Imports System.Diagnostics.Process
+Imports System.ComponentModel
 Imports System.Math
 Imports System.Net
 Imports System.Windows.Forms
 Imports System.Windows.Forms.Keys
 
+''' <summary>
+''' Form hiển thị tiến độ tải bộ cài mới và khởi chạy installer sau khi tải thành công.
+''' </summary>
 Public Class FrmUpdate
 #Region "Fields"
-    Private ReadOnly _wc As New WebClient
+    Private ReadOnly _wc As New WebClient()
+    Private _downloadSucceeded As Boolean
 #End Region
 
 #Region "Overridden"
-    ' Hide sub window
+    ''' <summary>
+    ''' Ẩn form khỏi danh sách Alt+Tab để hộp cập nhật hoạt động như sub window.
+    ''' </summary>
     Protected Overrides ReadOnly Property CreateParams() As CreateParams
         Get
             Dim cp As CreateParams = MyBase.CreateParams
@@ -19,54 +25,101 @@ Public Class FrmUpdate
         End Get
     End Property
 
-    ' Deny Alt+F4
+    ''' <summary>
+    ''' Chặn Alt+F4 trong lúc cập nhật để tránh hủy tải không chủ ý.
+    ''' </summary>
     Protected Overrides Function ProcessCmdKey(ByRef msg As Message, keyData As Keys) As Boolean
         Return keyData = (Alt Or F4) OrElse MyBase.ProcessCmdKey(msg, keyData)
     End Function
 #End Region
 
 #Region "Events"
-    ' Load frm
+    ''' <summary>
+    ''' Khởi tạo trạng thái hiển thị khi form cập nhật được load.
+    ''' </summary>
     Private Sub FrmUpdate_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         lblCapacity.Text = ""
         lblPercent.Text = ""
         pnlProgressBar.Width = 1
     End Sub
 
-    ' Shown frm
+    ''' <summary>
+    ''' Chuẩn bị thư mục tạm và bắt đầu tải bộ cài mới.
+    ''' </summary>
     Private Sub FrmUpdate_Shown(sender As Object, e As EventArgs) Handles MyBase.Shown
         FIFrm()
         CrtDirAdv(FRNT_PATH)
         DelFileAdv(FILE_SETUP_ADR)
-        tmrMain.StrtAdv()
         AddHandler _wc.DownloadProgressChanged, AddressOf Upd_DownloadProgressChanged
-        _wc.DownloadFileAsync(New Uri(_wc.DownloadString(My.Resources.link_app)), FILE_SETUP_ADR)
+        AddHandler _wc.DownloadFileCompleted, AddressOf Upd_DownloadFileCompleted
+
+        Try
+            Dim setupUrl As String = _wc.DownloadString(My.Resources.link_app)
+            _wc.DownloadFileAsync(New Uri(setupUrl), FILE_SETUP_ADR)
+        Catch ex As Exception
+            ErrSty($"{vbCrLf}Không thể tải bản cập nhật: {ex.Message}{vbCrLf}")
+            Close()
+        End Try
     End Sub
 
-    ' Update download progress
+    ''' <summary>
+    ''' Cập nhật dung lượng, phần trăm và thanh tiến độ khi WebClient báo tiến trình tải.
+    ''' </summary>
     Private Sub Upd_DownloadProgressChanged(sender As Object, e As DownloadProgressChangedEventArgs)
         lblCapacity.Text = String.Format("{0} MB / {1} MB", (e.BytesReceived / 1024D / 1024D).ToString("0.00"), (e.TotalBytesToReceive / 1024D / 1024D).ToString("0.00"))
         lblPercent.Text = $"{e.ProgressPercentage}%"
-        pnlProgressBar.Width = CInt(Ceiling(e.ProgressPercentage * Width / 100D))
+
+        Dim progressWidth As Integer = CInt(Ceiling(e.ProgressPercentage * pnlMain.ClientSize.Width / 100D))
+        pnlProgressBar.Width = Min(pnlMain.ClientSize.Width, Max(1, progressWidth))
     End Sub
 
-    ' tmr main
+    ''' <summary>
+    ''' Đóng form sau khi tải xong, hoặc ghi lỗi nếu quá trình tải thất bại.
+    ''' </summary>
+    Private Sub Upd_DownloadFileCompleted(sender As Object, e As AsyncCompletedEventArgs)
+        If e.Cancelled Then
+            ErrSty($"{vbCrLf}Quá trình tải bản cập nhật đã bị hủy.{vbCrLf}")
+        ElseIf e.Error IsNot Nothing Then
+            ErrSty($"{vbCrLf}Tải bản cập nhật thất bại: {e.Error.Message}{vbCrLf}")
+        Else
+            _downloadSucceeded = True
+            lblPercent.Text = "100%"
+            pnlProgressBar.Width = pnlMain.ClientSize.Width
+        End If
+
+        tmrMain.StopAdv()
+        Close()
+    End Sub
+
+    ''' <summary>
+    ''' Dự phòng cho timer cũ: chỉ đóng form khi download đã hoàn tất thành công.
+    ''' </summary>
     Private Sub TmrMain_Tick(sender As Object, e As EventArgs) Handles tmrMain.Tick
-        If lblPercent.Text = "100%" Then
+        If _downloadSucceeded Then
             tmrMain.StopAdv()
             Close()
         End If
     End Sub
 
-    ' Closing frm
+    ''' <summary>
+    ''' Chạy hiệu ứng fade out khi form cập nhật đóng.
+    ''' </summary>
     Private Sub FrmUpdate_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
         FOFrm()
     End Sub
 
-    ' Closed frm
+    ''' <summary>
+    ''' Dọn WebClient và chỉ chạy installer nếu file cập nhật đã tải thành công.
+    ''' </summary>
     Private Sub FrmUpdate_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
-        Start(FILE_SETUP_ADR)
-        KillPrcs(My.Resources.app_name)
+        RemoveHandler _wc.DownloadProgressChanged, AddressOf Upd_DownloadProgressChanged
+        RemoveHandler _wc.DownloadFileCompleted, AddressOf Upd_DownloadFileCompleted
+        _wc.Dispose()
+
+        If _downloadSucceeded Then
+            System.Diagnostics.Process.Start(FILE_SETUP_ADR)
+            KillPrcs(My.Resources.app_name)
+        End If
     End Sub
 #End Region
 End Class
--- Ibaraki Pca Weight/Script/Main.vb
+++ Ibaraki Pca Weight/Script/Main.vb
@@ -1,31 +1,40 @@
-﻿Imports System.Console
+Imports System.Console
 Imports System.Text.Encoding
 Imports System.Windows.Forms
 Imports System.Windows.Forms.DialogResult
 Imports System.Windows.Forms.MessageBox
 Imports System.Windows.Forms.MessageBoxButtons
 
+''' <summary>
+''' Entry point của ứng dụng console/WinForms dùng để kiểm tra license rồi chạy luồng xử lý Excel.
+''' </summary>
 Public Module Main
     ''' <summary>
-    ''' Main.
+    ''' Thiết lập encoding UTF-8, xác thực license nếu cần và khởi động ứng dụng chính.
     ''' </summary>
     Public Sub Main()
         OutputEncoding = UTF8
-        If Not My.Settings.Chk_Key Then
-ChkPt:
-            If InputBox("シリアルを入力", "ライセンスキー") = My.Resources.key_ser Then
-                UpdVldLic()
-                RunApp()
-            Else
-                If Show("ライセンスが間違っています！", "エラー", RetryCancel, MessageBoxIcon.Error) = Retry Then
-                    GoTo ChkPt
-                Else
-                    ErrSty("終了するには、任意のキーを押してください...")
-                    ReadKey()
-                End If
-            End If
-        Else
+        If My.Settings.Chk_Key OrElse ValidateLicense() Then
             RunApp()
         End If
     End Sub
+
+    ''' <summary>
+    ''' Nhận serial từ người dùng và cho phép thử lại đến khi đúng hoặc người dùng hủy.
+    ''' </summary>
+    ''' <returns>True nếu license hợp lệ, ngược lại là False.</returns>
+    Private Function ValidateLicense() As Boolean
+        Do
+            If InputBox("シリアルを入力", "ライセンスキー") = My.Resources.key_ser Then
+                UpdVldLic()
+                Return True
+            End If
+
+            If Show("ライセンスが間違っています！", "エラー", RetryCancel, MessageBoxIcon.Error) <> Retry Then
+                ErrSty("終了するには、任意のキーを押してください...")
+                ReadKey()
+                Return False
+            End If
+        Loop
+    End Function
 End Module
